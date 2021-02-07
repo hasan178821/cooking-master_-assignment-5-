c4fb8814e93baab6b60b5a91852354e601739438
hasan178821 <hasanrana178821@gmail.com>
2021-02-08 03:26:51 +0600

    show one card

--- cooking.js
+++ cooking.js
@@ -1,48 +1,46 @@
-// document.getElementById("searchButton").addEventListener("click", function() {
-//     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-//         .then(res => res.json())
-//         .then(data => {
-//             const foodCard = document.getElementById("foodCards");
+/* // let getInputValue = document.getElementById("searchInput").value.trim();
+fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=a`)
+    .then(res => res.json())
+    .then(data => displayAllFoods(data));
 
-//             for (let i = 0; i < data.meals.length; i++) {
-//                 const card = data.meals[i];
-//                 const Image = document.createElement("img");
-//                 Image.src = data.meals[0].strMealThumb;
-//                 foodCard.appendChild(Image);
 
-//                 const h4 = document.createElement("h4");
-//                 h4.innerText = data.meals[0].strMeal;
-//                 foodCard.appendChild(h4);
-//             }
+const displayAllFoods = foodCard => {
+    const cardsFood = document.getElementById('foodCards');
+    foodCard.forEach(card => {
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'allItem';
 
-//             // const Image = document.createElement("img");
-//             // Image.src = data.meals[0].strMealThumb;
-//             // foodCard.appendChild(Image);
+        const foodInfo = `
+        <img src="${meals.strMealThumb}">
+        <h4>${meals.strMeal}</h4>
+        `;
 
-//             // const h4 = document.createElement("h4");
-//             // h4.innerText = data.meals[0].strMeal;
-//             // foodCard.appendChild(h4);
+        foodDiv.innerHTML = foodInfo;
+        cardsFood.appendChild(foodDiv);
 
-//         })
-// })
+    });
+} */
 
+
+
+const searchInputValue = document.getElementById("searchInput");
 document.getElementById("searchButton").addEventListener("click", function() {
-    let getInputValue = document.getElementById("searchInput").value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${getInputValue}`)
-        .then(res => res.json())
-        .then(data => {
-            const getData = data.meals;
-            for (let i = 0; i < getData.length; i++) {
-                const card = getData[i];
-                const Image = document.createElement("img");
-                Image.src = "${getData.strMealThumb}";
-                foodCards.appendChild(Image);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchInputValue.value}`)
+        .then(response => response.json())
+        .then(data => displayAllFoods(data));
 
-                const h4 = document.createElement("h4");
-                h4.innerText = "${getData.strMeal}";
-                foodCards.appendChild(h4);
-            }
-        });
+    const displayAllFoods = allFoods => {
+        // console.log(allFoods.meals[0].strMeal);
+        const foodDiv = document.getElementById("foodCards");
 
+        const image = document.createElement("img");
+        image.src = `${allFoods.meals[0].strMealThumb}`;
+        foodDiv.appendChild(image);
+
+        const h4 = document.createElement("h4");
+        h4.innerText = `${allFoods.meals[0].strMeal}`;
+        foodDiv.appendChild(h4);
+
+    }
 
 })
